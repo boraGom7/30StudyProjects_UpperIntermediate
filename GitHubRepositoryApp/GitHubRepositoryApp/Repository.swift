84ef453a3ce7a638f1e7235fb31421e1a92d41c8
boraGom7 <83940751+boraGom7@users.noreply.github.com>
2022-01-06 14:34:55 +0900

    Add files via upload

---
+++ GitHubRepositoryApp/GitHubRepositoryApp/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GitHubRepositoryApp
+//
+//  Created by 임혜정 on 2022/01/06.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
---
+++ GitHubRepositoryApp/GitHubRepositoryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubRepositoryApp
+//
+//  Created by 임혜정 on 2022/01/04.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
